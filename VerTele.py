e3fdb4f4ce394c57a2e67698e88397cc44825152
madkoding <madkoding@gmail.com>
2026-02-19 01:12:40 -0300

    refactor: simplify entry point to single import

--- VerTele.py
+++ VerTele.py
@@ -1,52 +1,12 @@
-#!/usr/bin/python
-from gi.repository import Gtk
-from subprocess import Popen
-import xml.etree.ElementTree as ET
-tree = ET.parse('channelList.xml')
-root = tree.getroot()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+VerTele - Visor de Canales
+Punto de entrada principal
+"""
 
+from app import main
+import sys
 
-class MyWindow(Gtk.Window):
-
-    def __init__(self):
-        Gtk.Orientation.VERTICAL
-        Gtk.Window.__init__(self, title="Canales")
-        self.set_border_width(10)
-        self.set_default_size(200, 200)
-        box = Gtk.Box(spacing=6)
-        box.set_orientation(Gtk.Orientation.VERTICAL)
-        self.add(box)
-
-        for channel in root.findall('channel'):
-            button = Gtk.Button(label=channel.get('title'))
-            button.instance = channel.get('instance')
-            button.connect("clicked", self.on_button_clicked)
-            box.pack_start(button, True, True, 0)
-
-    def on_button_clicked(ventana, widget):
-        findelement = root.find('channel[@instance="'+widget.instance+'"]')
-        if findelement is None:
-            print "Channel not found"
-        else:
-            pageUrl = findelement.find('pageUrl').text
-            swfUrl = findelement.find('swfUrl').text
-            rtmp = findelement.find('rtmp').text
-            playpath = findelement.find('playpath').text
-            videoTitle = findelement.get('title')
-
-            try:
-                Popen(
-                    "rtmpdump"
-                    + ' --pageUrl "' + pageUrl + '"'
-                    + ' --swfUrl "' + swfUrl + '"'
-                    + ' --rtmp "' + rtmp + '"'
-                    + ' --playpath "' + playpath + '"'
-                    + ' --live --flv - | cvlc --video-title="'+videoTitle+'" -',
-                    shell=True)
-            except OSError as e:
-                print >> "Execution failed:", e
-
-win = MyWindow()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+if __name__ == "__main__":
+    sys.exit(main())
